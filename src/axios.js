624dee6b07d12f8c9c24f0cbdfd83b3fd1ed3870
DoctorSpace <jackassdimas1@gmail.com>
2023-09-02 16:42:28 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4444'
+    // baseURL: 'http://localhost:4444'
+    baseURL: 'http://185.46.8.247:4444'
 })
 
 export default instance
