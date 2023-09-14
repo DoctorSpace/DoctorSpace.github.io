238a0cf91faa60e63246e75b9e8b7f18b0d14fee
DoctorSpace <jackassdimas1@gmail.com>
2023-09-15 02:36:26 +0300

    Yandex WebMaster

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import MainPage from '../MainPage/MainPage';
 import AuthorizationPage from '../AuthorizationPage/AuthorizationPage'
 import ContactPage from '../ContactPage/ContactPage';
+import Yandex from '../Yandex/Yandex';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/Authorization' element={<AuthorizationPage />} />
         <Route path='*' element={<MainPage />} />
         <Route path='/Contact' element={<ContactPage />} />
+        <Route path='/yandex_2e9e41e634307037.html' element={<Yandex />} />
       </Routes>
     </div>
   );
---
+++ src/components/Yandex/Yandex.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Yandex = () => {
+    return (
+        <html>
+            <body>Verification: 2e9e41e634307037</body>
+        </html>
+    );
+};
+
+export default Yandex;
